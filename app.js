abe77f8db04e6b9eba96db052333cff54edd8300
robak <marek.kors@wp.pl>
2018-11-10 16:35:19 +0100

    add all

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Title
 const h1 = document.querySelector('h1');
 
 h1.addEventListener('mouseenter', changeTitle = () => {
@@ -6,12 +7,11 @@ h1.addEventListener('mouseenter', changeTitle = () => {
 h1.addEventListener('mouseleave', changeTitle = () => {
     h1.classList.remove('changeTitle');
 })
-const h3 = document.querySelector('h3');
-
+// Select color
 const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const hexBtn = document.querySelector('h3');
 
-const time = 2000;
+const time = 100;
 
 function getHex() {
     let hexCol = '#';
@@ -23,3 +23,81 @@ function getHex() {
     hexBtn.style.color = hexCol;
 }
 setInterval(getHex, time);
+
+// Game logic
+
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+}
+
+const game = {
+    playerHnd: "",
+    aiHand: "",
+}
+
+const hands = [...document.querySelectorAll('.select img')];
+
+function handSelection() {
+    game.playerHnd = this.dataset.option
+    console.log(game.playerHnd);
+    hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.boxShadow = '0 0 0 4px #665C84';
+}
+
+function aiChoise() {
+    return hands[Math.floor(Math.random() * 3)].dataset.option;
+}
+
+function checkResult(player, ai) {
+    console.log(player, ai)
+    if (player === ai) {
+        return 'draw'
+    } else if ((player === "paper" && ai === "rock") || (player === "rock" && ai === "scissors") || (player === "scissors" && ai === "paper")) {
+        return 'win'
+    } else {
+        return 'loss'
+    }
+}
+
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "you win!!!";
+        document.querySelector('[data-summary="who-win"]').style.color = "green"
+    } else if (result === "loss") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = "opponent win!!!";
+        document.querySelector('[data-summary="who-win"]').style.color = "red"
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "draw!!!";
+        document.querySelector('[data-summary="who-win"]').style.color = "black"
+    }
+}
+
+function endGame() {
+    document.querySelector(`[data-option="${game.playerHnd}"]`).style.boxShadow = "";
+    game.playerHnd = "";
+}
+
+function startGame() {
+    if (!game.playerHnd) return alert("please select");
+    game.aiHand = aiChoise()
+    const gameResult = checkResult(game.playerHnd, game.aiHand);
+    console.log(gameResult);
+    publishResult(game.playerHnd, game.aiHand, gameResult);
+    endGame();
+}
+
+hands.forEach(hand => hand.addEventListener('click', handSelection))
+
+document.querySelector('.start').addEventListener('click', startGame)
